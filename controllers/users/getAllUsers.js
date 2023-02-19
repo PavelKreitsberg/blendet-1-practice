03429347cde7b0e83d1f85647d2e8ab9f80c8f2e
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2023-02-19 12:51:13 +0200

    update

--- controllers/users/getAllUsers.js
+++ controllers/users/getAllUsers.js
@@ -3,3 +3,5 @@ const User = require("../../models/User");
 const getAllUsers = async (req, res) => {
   return await User.find({ _id: { $ne: req.user._id } });
 };
+
+module.exports = getAllUsers;
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const getAllUsersCtrl = require("../controllers/users/getAllUsers");
-const loginCtrl = require("../controllers/users/loginCtrl");
-const registerCtrl = require("../controllers/users/registerCtrl");
-const logoutCtrl = require("../controllers/users/logoutCtrl");
+const loginCtrl = require("../controllers/users/login");
+const registerCtrl = require("../controllers/users/register");
+const logoutCtrl = require("../controllers/users/logout");
 
 router.post("/login", loginCtrl);
 
